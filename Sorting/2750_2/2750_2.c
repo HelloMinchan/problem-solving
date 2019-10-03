abad1885237c2aa683c6568dd652a1cdc0fdc2f9
HelloMinchan <wjdalscksdle@naver.com>
2019-10-03 16:09:40 +0900

    2019.10.03

--- Sorting/2750_2/2750_2.c
+++ Sorting/2750_2/2750_2.c
@@ -3,24 +3,21 @@
 #define SWAP(a, b) {a^=b, b^=a, a^=b;}
 
 int main(void) {
-    int N = 0, min = 0, index = 0;
+    int N = 0, min = 0;
     scanf("%d", &N);
     int *arr = (int *)malloc(sizeof(int)*N);
 
     for (int i = 0; i < N; i++)
 	scanf("%d", &arr[i]);
 
-    for (int i = 0; i < N; i++) {
-	min = arr[i];
-	index = i;
+    for (int i = 0; i < N - 1; i++) {
+	min = i;
 	for (int j = i + 1; j < N; j++) {
-	    if (min > arr[j]) {
-		min = arr[j];
-		index = j;
-	    }
+	    if (arr[min] > arr[j])
+		min = j;
 	}
-	if (i != index)
-	    SWAP(arr[i], arr[index]);
+	if(min != i)
+	    SWAP(arr[i], arr[min]);
     }
 
     for (int i = 0; i < N; i++)
