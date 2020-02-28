8df99088edee67e4cf63ac455a6c69346ac0550e
HelloMinchan <wjdalscksdle@naver.com>
2020-02-29 00:02:26 +0900

    2020.02.29

---
+++ 1316/1316.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+tot = int(input())
+for _ in range(tot):
+    visit = [False] * 26
+    word = list(input().rstrip())
+    pre = word[0]
+    for i in word:
+        if visit[ord(i) - 97] == True:
+            tot -= 1
+            break
+        else:
+            if pre == i:
+                pre = i
+            else:
+                visit[ord(pre) - 97] = True
+
+print(tot)
