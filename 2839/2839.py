36ec4187fa5ec5c89c0c79cfbc7d79adf4377950
HelloMinchan <wjdalscksdle@naver.com>
2020-03-13 11:12:21 +0900

    2020.03.13

--- 2839/2839.py
+++ 2839/2839.py
@@ -3,3 +3,22 @@ input = sys.stdin.readline
 
 count = 0
 n = int(input())
+
+count += n // 5
+temp = n % 5
+
+while temp != n:
+    if temp % 3 != 0:
+        temp += 5
+        count -= 1
+    else:
+        count += temp // 3
+        temp = n % 3
+        print(count)
+        exit()
+
+if temp % 3 == 0:
+    count += temp // 3
+    print(count)
+else:
+    print(-1)
