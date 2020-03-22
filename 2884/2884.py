f749ce572011a35b6481e183381f90ba54067de2
HelloMinchan <wjdalscksdle@naver.com>
2020-03-22 19:17:15 +0900

    2020.03.22

--- 2884/2884.py
+++ 2884/2884.py
@@ -1,12 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-t, m = map(int, input().split())
+h, m = map(int, input().split())
 
 if m - 45 >= 0:
-    print(t, m - 45)
+    print(h, m - 45)
 else:
-    if t == 0:
+    if h == 0:
         print(23, 60 - (45 - m))
     else:
-        print(t - 1, 60 - (45 - m))
+        print(h - 1, 60 - (45 - m))
