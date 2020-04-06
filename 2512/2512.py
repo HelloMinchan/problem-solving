9926dee18aef970672cec5153827737bd7675faf
HelloMinchan <wjdalscksdle@naver.com>
2020-04-06 23:03:53 +0900

    2020.04.06

---
+++ 2512/2512.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+inquiredBudget = list(map(int, input().split()))
+totalBudget = int(input())
+result = 0
+left = 0
+right = max(inquiredBudget)
+
+while left <= right:
+    assignedBudget = 0
+    mid = (left + right) // 2
+
+    for budget in inquiredBudget:
+        if budget - mid < 0:
+            assignedBudget += budget
+            continue
+        assignedBudget += mid
+    
+    if totalBudget < assignedBudget:
+        right = mid - 1
+    else:
+        print(result)
+        result = mid
+        left = mid + 1
+
+print(result)
