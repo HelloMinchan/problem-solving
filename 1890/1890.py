d4bf6c87540d3b77173e28c735da26bb5d786f2b
HelloMinchan <wjdalscksdle@naver.com>
2020-04-20 12:37:24 +0900

    2020.04.20

---
+++ 1890/1890.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+gameBoard = [list(map(int , input().split())) for _ in range(N)]
+memoization = [[0] * N for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        ii = i
+        jj = j
+        
+        for x in range(ii):
+            if gameBoard[x][j] == i - x:
+                if not x and not j:
+                    memoization[i][j] = 1
+                    break
+                if memoization[x][j]:
+                    memoization[i][j] += memoization[x][j]
+
+        for y in range(jj):
+            if gameBoard[i][y] == j - y:
+                if not y and not i:
+                    memoization[i][j] = 1
+                    break
+                if memoization[i][y]:
+                    memoization[i][j] += memoization[i][y]
+
+print(memoization[N - 1][N - 1])
