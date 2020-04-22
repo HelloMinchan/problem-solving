7a86a0e0ac83764306ac5d98b78ba0d129577054
HelloMinchan <wjdalscksdle@naver.com>
2020-04-22 14:06:53 +0900

    2020.04.22

--- 9252/9252.cpp
+++ 9252/9252.cpp
@@ -48,7 +48,7 @@ int main(void)
 
     for (int i = N - 1; i > 0; i--)
     {
-        for (int j = 0; j < M; j++)
+        for (int j = 1; j < M; j++)
         {
             if (memoization[i][j] == lcsLength)
             {
