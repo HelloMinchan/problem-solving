eef6e2812b7eefda8d1efc09894a3b6852e3253b
HelloMinchan <wjdalscksdle@naver.com>
2020-04-24 14:59:40 +0900

    2020.04.24

--- 1915/1915.cpp
+++ 1915/1915.cpp
@@ -8,7 +8,8 @@ using namespace std;
 
 int arr[1001][1001];
 
-int main(void) {
+int main(void)
+{
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
@@ -17,14 +18,15 @@ int main(void) {
 	cin >> n >> m;
 
 	string temp = "";
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
+	{
 		temp = "";
 		cin >> temp;
 		for (int j = 0; j < m; j++)
 			arr[i][j] = temp[j] - '0';
 	}
-	
-	int dx[4] = { -1,0,-1 }, dy[4] = { 0,-1,-1 };
+
+	int dx[4] = {-1, 0, -1}, dy[4] = {0, -1, -1};
 	int maxLength = 0;
 
 	vector<int> length;
@@ -33,18 +35,22 @@ int main(void) {
 	int ii = 0;
 	int jj = 0;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] == 1) {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (arr[i][j] == 1)
+			{
 				length.clear();
 
-				for (int way = 0; way < 3; way++) {
+				for (int way = 0; way < 3; way++)
+				{
 					ii = i + dx[way];
 					jj = j + dy[way];
 
-					if (ii < 0 or ii > n - 1 or jj < 0 or jj > m - 1)
+					if (ii < 0 || ii > n - 1 || jj < 0 || jj > m - 1)
 						continue;
-					if (arr[ii][jj] and arr[ii][jj] != 0)
+					if (arr[ii][jj] && arr[ii][jj] != 0)
 						length.push_back(arr[ii][jj]);
 
 					countLength = length.size();
@@ -57,13 +63,13 @@ int main(void) {
 
 				if (countLength == 3)
 					arr[i][j] = minLength + 1;
-				
+
 				if (maxLength < arr[i][j])
 					maxLength = arr[i][j];
-			}	
+			}
 		}
 	}
-	
+
 	cout << pow(maxLength, 2);
 
 	return 0;
