86da0cd874c7595ca45b6332ef897a54b65b902b
HelloMinchan <wjdalscksdle@naver.com>
2020-05-15 17:45:59 +0900

    2020.05.15

--- 11003/11003.py
+++ 11003/11003.py
@@ -1,14 +1,18 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 
 N, L = map(int, input().split())
 A = list(map(int, input().split()))
-window = []
+dq = deque()
+
 for i in range(N):
-    if i < L:
-        window.append(A[i])
-        print(min(window), end=" ")
-        continue
-    window.append(A[i])
-    del window[0]
-    print(min(window), end=" ")
+    while dq and dq[-1][1] > A[i]:
+        dq.pop()
+    
+    dq.append((i, A[i]))
+
+    while dq and dq[0][0] < i - L + 1:
+        dq.popleft()
+        
+    print(dq[0][1], end=" ")
