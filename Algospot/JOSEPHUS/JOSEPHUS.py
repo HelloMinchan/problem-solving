2aa0a08728747c4b965a85e09a5c8991747fed2a
HelloMinchan <wjdalscksdle@naver.com>
2020-05-29 13:14:45 +0900

    2020.05.29

---
+++ Algospot/JOSEPHUS/JOSEPHUS.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+C = int(input())
+
+for _ in range(C):
+    N, K = map(int, input().split())
+    soldiers = [i + 1 for i in range(N)]
+
+    i = 0
+    while len(soldiers) != 2:
+        del soldiers[i]
+        i += K - 1
+        if i > len(soldiers) - 1:
+            i -= len(soldiers)
+        i %= len(soldiers)
+    
+    print(*soldiers)
