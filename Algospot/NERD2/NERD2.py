ad52b21d4959ebd22cc149032b3c39d38242dce8
HelloMinchan <wjdalscksdle@naver.com>
2020-06-03 23:50:13 +0900

    2020.06.03

--- Algospot/NERD2/NERD2.py
+++ Algospot/NERD2/NERD2.py
@@ -1,31 +1,32 @@
-import sys
+import sys, bisect
 input = sys.stdin.readline
 
 C = int(input())
 
 for _ in range(C):
     ans = 0
-    N = int(input())
-    people = [list(map(int,input().split())) for _ in range(N)]
     nerds = []
     numOfNerd = 0
+    N = int(input())
+    
+    for _ in range(N):  
+        p, q = map(int, input().split())
 
-    for p, q in people:
-        if not len(nerds):
-            nerds.append((p,q))
-            ans = numOfNerd = 1
-            continue
+        i = bisect.bisect(nerds, (p, q))
 
-        for i, nerd in enumerate(nerds):
-            if p > nerd[0] or q > nerd[1]:
-                if p > nerd[0] and q > nerd[1]:
-                    nerds.pop(i)
-                    nerds.insert(i,(p,q))
-                    break
-        else:
-            nerds.append((p,q))
+        if i == numOfNerd or q > nerds[i][1]:
+            nerds.insert(i , (p, q))
             numOfNerd += 1
             
+            i -= 1
+            while i >= 0:
+                if q <= nerds[i][1]:
+                    break
+                
+                nerds.pop(i)
+                numOfNerd -= 1
+                i -= 1
+                
         ans += numOfNerd
 
     print(ans)
