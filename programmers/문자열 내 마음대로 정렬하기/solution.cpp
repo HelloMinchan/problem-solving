b413df9d6a7d1611e265e853d99bcfbd4af534f3
HelloMinchan <wjdalscksdle@naver.com>
2020-06-12 00:30:08 +0900

    2020.06.12

---
+++ programmers/문자열 내 마음대로 정렬하기/solution.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int i;
+
+bool compare(const string &a, const string &b)
+{
+    if (a[i] == b[i])
+        return a < b;
+    else
+        return a[i] < b[i];
+}
+
+vector<string> solution(vector<string> strings, int n)
+{
+    i = n;
+
+    sort(strings.begin(), strings.end(), compare);
+
+    return strings;
+}
