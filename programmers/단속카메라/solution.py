a84f4fbb67a9c3f3a4e206ed59d6600bfeef24d1
HelloMinchan <wjdalscksdle@naver.com>
2020-06-24 02:36:49 +0900

    2020.06.24

---
+++ programmers/단속카메라/solution.py
@@ -0,0 +1,12 @@
+def solution(routes):
+    answer = 0
+    
+    camera = -30000
+    routes.sort(key=lambda x: x[1])
+    
+    for route in routes:
+        if camera < route[0]:
+            answer += 1
+            camera = route[1]
+    
+    return answer
