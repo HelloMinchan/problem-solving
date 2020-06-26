9195dc0dd4ab85a2f630c9c00f5713be4ecec7af
HelloMinchan <wjdalscksdle@naver.com>
2020-06-26 10:37:59 +0900

    2020.06.26

--- BaekjoonOnlineJudge/9471/9471.py
+++ BaekjoonOnlineJudge/9471/9471.py
@@ -2,21 +2,19 @@ import sys
 input = sys.stdin.readline
 
 
-def pisano(M):
+def pisano():
     back2 = 0
-    back1 = 1
-    now = 0
+    back1 = 0
+    now = 1
     count = 0
 
     while 1:
-        now = back1 + back2 % M
-
-        back1 = back2
-        back2 = now
-
+        back2 = back1
+        back1 = now
+        now = (back1 + back2) % M
         count += 1
-        
-        if back1 == 0 and back2 == 1:
+
+        if back1 == 0 and now == 1:
             return count
 
 
@@ -25,4 +23,4 @@ P = int(input())
 for _ in range(P):
     N, M = map(int, input().split())
 
-    print(N, pisano(M))
+    print(N, pisano())
