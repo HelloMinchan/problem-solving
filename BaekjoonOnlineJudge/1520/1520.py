9b6ddb2488aa26e6c1c79a0939ada4d5ba520535
HelloMinchan <wjdalscksdle@naver.com>
2020-06-28 20:32:32 +0900

    2020.06.28

--- BaekjoonOnlineJudge/1520/1520.py
+++ BaekjoonOnlineJudge/1520/1520.py
@@ -1,15 +1,14 @@
 import sys
-sys.setrecursionlimit(10 ** 7)
 input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 7)
 
 
 def DFS(i, j):
-    if not i and not j:
-        return 1;
-    if (i < 0 or i >= N or j < 0 or j >= M):
-        return 0;
     if memoization[i][j] != -1:
-        return memoization[i][j];
+        return memoization[i][j]
+    
+    if i == N - 1 and j == M - 1:
+        return 1
     
     memoization[i][j] = 0
 
@@ -17,18 +16,19 @@ def DFS(i, j):
         ii = i + dx[way]
         jj = j + dy[way]
 
-        if ii < 0 or ii >= N or jj < 0 or jj >= M:
+        if ii < 0 or ii > N - 1 or jj < 0 or jj > M - 1:
             continue
         
-        if MAP[ii][jj] > MAP[i][j]:
+        if MAP[i][j] > MAP[ii][jj]:
             memoization[i][j] += DFS(ii, jj)
-
-    return memoization[i][j]
     
+    return memoization[i][j]
+
 
 N, M = map(int, input().split())
+
 MAP = [list(map(int, input().split())) for _ in range(N)]
 memoization = [[-1] * M for _ in range(N)]
-dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
+dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
 
-print(DFS(N - 1, M - 1))
+print(DFS(0, 0))
