dea8980d94662934fb071e6af22ade0095c093fd
HelloMinchan <wjdalscksdle@naver.com>
2020-07-01 13:41:35 +0900

    2020.07.01

--- BaekjoonOnlineJudge/1726/1726.py
+++ BaekjoonOnlineJudge/1726/1726.py
@@ -15,7 +15,7 @@ def opp(n):
 
 def BFS(x, y, di, order):
     hq = []
-    visit[x][y][di] = True
+    visit[di][x][y] = True
     heapq.heappush(hq, (order, x, y, di))
 
     while hq:
@@ -34,24 +34,24 @@ def BFS(x, y, di, order):
             if factory[ii][jj]:
                 break
             
-            if not visit[ii][jj][di]:
-                visit[ii][jj][di] = True
+            if not visit[di][ii][jj]:
+                visit[di][ii][jj] = True
                 heapq.heappush(hq, (order + 1, ii, jj, di))
 
         for way in range(1, 5):
             if way == di:
                 continue
                 
-            if not visit[i][j][way]:
+            if not visit[way][i][j]:
                 if di == way or di == opp(way):
                     continue
-                visit[i][j][way] = True
+                visit[way][i][j] = True
                 heapq.heappush(hq, (order + 1, i, j, way))
 
    
 N, M = map(int, input().split())
 factory = [list(map(int, input().split())) for _ in range(N)]
-visit = [[[False] * 5 for _ in range(102)] for _ in range(102)]
+visit = [[[False] * M for _ in range(N)] for _ in range(5)]
 
 # 공백 : 0, 동 : 1, 서 : 2, 남 : 3, 북 : 4 
 dx , dy = [0, 0, 0, 1, -1], [0, 1, -1, 0, 0]
