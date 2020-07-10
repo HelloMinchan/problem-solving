2b07172c6494edd5ed1d87fa264143dfc10ce85b
HelloMinchan <wjdalscksdle@naver.com>
2020-07-10 19:04:10 +0900

    2020.07.10

--- BaekjoonOnlineJudge/14725/14725.py
+++ BaekjoonOnlineJudge/14725/14725.py
@@ -22,10 +22,21 @@ class Trie():
                 curr_node.children[char] = Node(char)
             curr_node = curr_node.children[char]
 
-    def search(self, L):
-        curr_node = self.head
-
-        print("--" * L, end="")
+    def printStructure(self, L, curr_node):
+        if L == 0:
+            curr_node = self.head
+        
+        for child in sorted(curr_node.children.keys()):
+            print("--" * L, child, sep="")
+            self.printStructure(L + 1, curr_node.children[child])
 
         
 trie = Trie()
+
+N = int(input())
+
+for _ in range(N):
+    temp = list(input().split())
+    trie.insert(temp[1:])
+
+trie.printStructure(0, None)
