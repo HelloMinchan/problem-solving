8fd3a468f30e02c76d1bfbff236da3debeb019d0
HelloMinchan <wjdalscksdle@naver.com>
2020-07-11 22:48:01 +0900

    2020.07.11

--- BaekjoonOnlineJudge/2188/2188.py
+++ BaekjoonOnlineJudge/2188/2188.py
@@ -1,14 +1,39 @@
 import sys
 input = sys.stdin.readline
 
+
+def DFS(a):
+    visit[a] = True
+
+    for b in adjList[a]:
+        if B[b] == -1 or not visit[B[b]] and DFS(B[b]):
+            A[a] = b
+            B[b] = a
+            return True
+    
+    return False
+
+
+MAX = 200
+
 N, M = map(int, input().split())
 
-adjList = [[] for _ in range(N)]
+A = [-1] * 201
+B = [-1] * 201
+adjList = [[] for _ in range(MAX)]
 
 for i in range(N):
     temp = list(map(int, input().split()))
-    
-    for j in range(1, len(temp)):
-        adjList[i].append(temp[j])
 
-print(adjList)
+    for j in range(1, len(temp)):
+        adjList[i].append(temp[j] - 1)
+
+match = 0
+
+for i in range(N):
+    if A[i] == -1:
+        visit = [False] * MAX
+        if DFS(i):
+            match += 1
+
+print(match)
