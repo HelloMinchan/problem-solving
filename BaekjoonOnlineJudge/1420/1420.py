aa181cb4571832c7f99539dd5cfe3b8aa71d2abd
HelloMinchan <wjdalscksdle@naver.com>
2020-07-15 19:09:46 +0900

    2020.07.15

--- BaekjoonOnlineJudge/1420/1420.py
+++ BaekjoonOnlineJudge/1420/1420.py
@@ -1,14 +1,110 @@
+from collections import defaultdict, deque
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-city = [list(map(int, input().split())) for _ in range(N)]
+MAX_N = 20002
+INF = 2147483647
 
-K = H = 0
+N, M = map(int, input().split())
+city = [list(input().rstrip()) for _ in range(N)]
+adjList = [[] for _ in range(MAX_N)]
+
+capacity = defaultdict(lambda: defaultdict(int))
+flow = defaultdict(lambda: defaultdict(int))
+
+S = -1
+T = -1
+cnt = 0
+sx = sy = tx = ty = 0
 
 for i in range(N):
     for j in range(M):
         if city[i][j] == 'K':
-            K = (i, j)
+            S = cnt + 1
+            sx = j
+            sy = i
         elif city[i][j] == 'H':
-            H = (i, j)
+            T = cnt
+            tx = j
+            ty = i
+        
+        cnt += 2
+
+if (N == 1 and M == 1) or S == -1 or T == -1 or abs(sx - tx) + abs(sy - ty) == 1:
+    print(-1)
+    sys.exit(0)
+
+for i in range(N * M):
+    capacity[2 * i][2 * i + 1] = 1
+    adjList[2 * i].append(2 * i + 1)
+
+    capacity[2 * i + 1][2 * i] = 0
+    adjList[2 * i + 1].append(2 * i)
+
+sv = 0;
+for i in range(N):
+    for j in range(M):
+        if j + 1 < M and city[i][j] != '#' and city[i][j + 1] != '#':
+            dv = sv + 2;
+
+            capacity[sv + 1][dv] = INF;
+            capacity[dv][sv + 1] = 0;
+
+            capacity[dv + 1][sv] = INF;
+            capacity[sv][dv + 1] = 0;
+
+            adjList[sv + 1].append(dv);
+            adjList[dv].append(sv + 1);
+
+            adjList[dv + 1].append(sv);
+            adjList[sv].append(dv + 1);
+
+        if i + 1 < N and city[i][j] != '#' and city[i + 1][j] != '#':
+            dv = 2 * M + sv;
+
+            capacity[sv + 1][dv] = INF;
+            capacity[dv][sv + 1] = 0;
+
+            capacity[dv + 1][sv] = INF;
+            capacity[sv][dv + 1] = 0;
+
+            adjList[sv + 1].append(dv);
+            adjList[dv].append(sv + 1);
+
+            adjList[dv + 1].append(sv);
+            adjList[sv].append(dv + 1);
+
+        sv += 2;
+
+totalFlow = 0
+
+while 1:
+    visit = [-1] * MAX_N
+
+    dq = deque()
+    visit[S] = True
+    dq.append(S)
+
+    while dq:
+        sv = dq.popleft()
+        
+        for dv in adjList[sv]:
+            if capacity[sv][dv] - flow[sv][dv] > 0 and visit[dv] == -1:
+                visit[dv] = sv
+                dq.append(dv)
+
+                if dv == T:
+                    break
+    
+    if visit[T] == -1:
+        break
+
+    i = T
+    while i != S:
+        flow[visit[i]][i] += 1
+        flow[i][visit[i]] -= 1
+        i = visit[i]
+    
+    totalFlow += 1
+
+print(totalFlow)
--- BaekjoonOnlineJudge/15686/15686.py
+++ BaekjoonOnlineJudge/15686/15686.py
@@ -1,30 +1,50 @@
-import sys
+import sys, copy
+from sre_constants import CHCODES
 input = sys.stdin.readline
-from itertools import combinations as C
 
-N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-allHouse = []
-allChicken = []
+
+def DFS(si):
+    if len(stack) == M:
+        closedChickenHouse.append(copy.deepcopy(stack))
+
+    for i in range(si, len(chickenHouse)):
+        if not visit[i]:
+            visit[i] = True
+            stack.append(chickenHouse[i])
+            DFS(i + 1)
+            visit[i] = False
+            stack.pop()
+
+
+N , M = map(int, input().split())
+city = [list(map(int, input().split())) for _ in range(N)]
+
+house = []
+chickenHouse = []
+closedChickenHouse = []
 
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == 1:
-            allHouse.append((i, j))
-        elif arr[i][j] == 2:
-            allChicken.append((i, j))
+        if city[i][j] == 1:
+            house.append((i, j))
+        elif city[i][j] == 2:
+            chickenHouse.append((i, j))
 
-allAliveChickenList = list(C(allChicken, M))
+visit = [False] * len(chickenHouse)
+stack = []
+DFS(0)
 
-solution = []
-for aliveChickenList in allAliveChickenList:
-    allDistance = 0
-    for house in allHouse:
-        distance = 9999
-        for chickenX, chickenY in aliveChickenList:
-            if distance > abs(house[0] - chickenX) + abs(house[1] - chickenY):
-                distance = abs(house[0] - chickenX) + abs(house[1] - chickenY)
-        allDistance += distance
-    solution.append(allDistance)
+minChickenDist = 2147483647
+for closedChickenHouse_coord in closedChickenHouse:
+    chickenDist = 0
+    for house_coord in house:
+        tempChickenDist = 2147483647
 
-print(min(solution))
+        for chickenHouse_coord in closedChickenHouse_coord:
+            tempChickenDist = min(tempChickenDist, abs(chickenHouse_coord[0] - house_coord[0]) + abs(chickenHouse_coord[1] - house_coord[1]))
+        
+        chickenDist += tempChickenDist
+        
+    minChickenDist = min(minChickenDist, chickenDist)
+
+print(minChickenDist)
