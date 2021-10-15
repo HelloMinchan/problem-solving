c5bc0fceb095b1ee07ba7261afa93316e4158bf3
hellominchan <wjdalscksdle@naver.com>
2021-10-16 00:50:45 +0900

    21.10.15

--- BaekjoonOnlineJudge/2562/2562.py
+++ BaekjoonOnlineJudge/2562/2562.py
@@ -1,12 +1,22 @@
-import sys, copy
+import sys
 input = sys.stdin.readline
 
-arr = []
-for i in range(9):
-    arr.append(int(input()))
+# 최댓값을 저장할 변수
+max_num = 0
+# 최댓값의 인덱스를 저장할 변수
+index = 0
 
-temp = copy.deepcopy(arr)
-arr.sort()
+# 9개의 숫자를 입력 받음
+for i in range(1, 10):
+    input_num = int(input())
 
-print(arr[-1])
-print(temp.index(arr[-1]) + 1)
+    # 최댓값 보다 입력 받은 숫자가 크다면
+    if max_num < input_num:
+        # 최댓값을 갱신함
+        max_num= input_num
+        # 현재 인덱스를 저장함
+        index = i
+
+# 답 출력
+print(max_num)
+print(index)
