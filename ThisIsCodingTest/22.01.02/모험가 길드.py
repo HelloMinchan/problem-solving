8ccbf3f22f507c782cadd61b61a48097db9c4ac2
hellominchan <wjdalscksdle@naver.com>
2022-01-02 23:27:17 +0900

    22.01.02

--- ThisIsCodingTest/22.01.02/모험가 길드.py
+++ ThisIsCodingTest/22.01.02/모험가 길드.py
@@ -1,29 +1,24 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-
 N = int(input())
-fear_queue = deque(sorted(map(int, input().split())))
-
+fear_list = list(sorted(map(int, input().split())))
 
 # 정답 변수
 answer = 0
-# 그룹의 인원수
+# 그룹의 인원 수
 group_count = 0
 
-while fear_queue:
-    print(fear_queue)
-    fear = fear_queue.popleft()
-    print("이번 공포도 ===>", fear)
-    if group_count == 0:
-        group_count = fear - 1
-    else:
-        group_count -= 1
-    print("그룹 카운트", group_count)
-    if group_count == 0:
+# 인원의 공포도 가져옴
+for fear in fear_list:
+    # 인원 수 추가
+    group_count += 1
+
+    # 인원 수가 공포도와 같으면
+    if group_count == fear:
+        # 그룹이 만들어짐
         answer += 1
-    
-    print("그룹 수",answer)
+        # 인원 수 초기화
+        group_count = 0
 
 print(answer)
