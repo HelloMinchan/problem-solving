b57c59d3c73e6f87ff23b4a51cc128974ff589db
hellominchan <wjdalscksdle@naver.com>
2022-01-02 21:15:41 +0900

    22.01.02

---
+++ ThisIsCodingTest/22.01.02/곱하기 혹은 더하시.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+numbers = list(map(int, input().rstrip()))
+
+print(numbers)
+
+answer = 0
+
+for number in numbers:
+    if answer == 0 or number in [0, 1]:
+        answer += number
+    else:
+        answer *= number
+
+print(answer)
+        
