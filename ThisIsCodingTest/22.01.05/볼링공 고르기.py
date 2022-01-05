0d0df49ccbe48e1c165fe73f45eed8f43b411621
hellominchan <wjdalscksdle@naver.com>
2022-01-05 22:19:31 +0900

    22.01.05

--- ThisIsCodingTest/22.01.05/볼링공 고르기.py
+++ ThisIsCodingTest/22.01.05/볼링공 고르기.py
@@ -4,4 +4,14 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 bowlings = list(map(int, input().split()))
 
-print(sum(range(1, N)) - (len(bowlings) - len(set(bowlings))))
+bucket = [0 for _ in range(11)]
+answer = 0
+
+for bowling in bowlings:
+    bucket[bowling] += 1
+
+for i in range(1, 10):
+    for j in range(i + 1, 11):
+        answer += bucket[i] * bucket[j]
+
+print(answer)
