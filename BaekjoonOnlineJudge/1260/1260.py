febea5b9f3535f59ac993b94e9f93c4fae34ab7c
hellominchan <wjdalscksdle@naver.com>
2022-01-15 21:34:02 +0900

    22.01.15

--- BaekjoonOnlineJudge/1260/1260.py
+++ BaekjoonOnlineJudge/1260/1260.py
@@ -1,48 +1,60 @@
-from collections import deque
+from collections import deque as Deque
 import sys
 input = sys.stdin.readline
 
+def initialize_visit_list(start_vertex):
+    global N
+    
+    visit_list = [False for _ in range(N+1)]
+    visit_list[start_vertex] = True
 
-def DFS(sv):
-    print(sv, end=' ')
-    for dv in adjList[sv]:
-        if not visit[dv]:
-            visit[dv] = True
-            DFS(dv)
+    return visit_list
 
+def DFS(start_vertex):
+    global stack
 
-def BFS(sv):
-    dq = deque()
-    visit[sv] = True
-    dq.append(sv)
+    for vertex in adjacent_list[start_vertex]:
+        if not visit_list[vertex]:
+            visit_list[vertex] = True
+            stack.append(vertex)
+            DFS(vertex)
 
-    while dq:
-        sv = dq.popleft()
-        print(sv, end=' ')
-        
-        for dv in adjList[sv]:
-            if not visit[dv]:
-                visit[dv] = True
-                dq.append(dv)
+def BFS(start_vertex):
+    global deque
+    answer = []
+
+    while deque:
+        start_vertex = deque.popleft()
+        answer.append(start_vertex)
+
+        for vertex in adjacent_list[start_vertex]:
+            if not visit_list[vertex]:
+                visit_list[vertex] = True
+                deque.append(vertex)
+    
+    print(*answer)  
 
 
 N, M, V = map(int, input().split())
 
-adjList = [[] for _ in range(N + 1)]
+adjacent_list = [[] for _ in range(N+1)]
 
 for _ in range(M):
-    sv, dv = map(int, input().split())
+    start_vertex, destination_vertex = map(int, input().split())
 
-    adjList[sv].append(dv)
-    adjList[sv].sort()
-    adjList[dv].append(sv)
-    adjList[dv].sort()
+    adjacent_list[start_vertex].append(destination_vertex)
+    adjacent_list[start_vertex].sort()
 
-visit = [False] * (N + 1)
-visit[V] = True
+    adjacent_list[destination_vertex].append(start_vertex)
+    adjacent_list[destination_vertex].sort()
+
+    
+
+visit_list = initialize_visit_list(V)
+stack = [V]
 DFS(V)
+print(*stack)
 
-print()
-
-visit = [False] * (N + 1)
+visit_list = initialize_visit_list(V)
+deque = Deque([V])
 BFS(V)
