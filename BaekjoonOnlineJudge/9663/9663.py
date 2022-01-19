cff7952f11fd06b71a8743727577283de395b133
hellominchan <wjdalscksdle@naver.com>
2022-01-20 02:06:35 +0900

    22.01.19

--- BaekjoonOnlineJudge/9663/9663.py
+++ BaekjoonOnlineJudge/9663/9663.py
@@ -1,24 +1,25 @@
-import sys
-input = sys.stdin.readline
-
-
-def DFS(L):
+def DFS(row):
     global answer
-    if L == N:
+    
+    if row == N:
         answer += 1
-        return
-
-    for i in range(N):
-        if not col[i] and not slash[i + L] and not backSlash[i - L + N - 1]:
-            col[i] = slash[i + L] = backSlash[i - L + N - 1] = True
-            DFS(L + 1)
-            col[i] = slash[i + L] = backSlash[i - L + N - 1] = False
+    
+    for col in range(N):
+        if not col_check[col] and not back_slash_direction_check[col + row] and not slash_direction_check[col - row + N - 1]:
+            col_check[col] = True
+            back_slash_direction_check[col + row] = True
+            slash_direction_check[col - row + N - 1] = True
+            DFS(row + 1)
+            col_check[col] = False
+            back_slash_direction_check[col + row] = False
+            slash_direction_check[col - row + N - 1] = False
 
 
 N = int(input())
-col = [False] * N
-slash = [False] * (2 * N - 1)
-backSlash = [False] * (2 * N - 1)
+
+col_check = [False for _ in range(N)]
+slash_direction_check = [False for _ in range(N * 2 - 1)]
+back_slash_direction_check = [False for _ in range(N * 2 - 1)]
 answer = 0
 
 DFS(0)
