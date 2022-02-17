51c53b59947f0b1c117cdc8439808fc7582cc4da
hellominchan <wjdalscksdle@naver.com>
2022-02-17 10:48:18 +0900

    22.02.17

--- programmers/124 나라의 숫자/solution.py
+++ programmers/124 나라의 숫자/solution.py
@@ -1,13 +1,10 @@
 def solution(n):
-    answer = ''
-    form = ['4', '1', '2']
-    
-    while n:
-        answer = form[n % 3] + answer
-        
-        if not n % 3:
-            n = n // 3 - 1
-        else:
-            n //= 3
-            
+    digits = ["1", "2", "4"]
+    answer = ""
+
+    while n > 0:
+        n = n - 1
+        answer = digits[n % 3] + answer
+        n //= 3
+
     return answer
