a8937ba89db83f02b986b4557249224602b6f2bd
hellominchan <wjdalscksdle@naver.com>
2022-03-04 14:59:42 +0900

    22.03.04

--- programmers/동명 동물 수 찾기/solution.sql
+++ programmers/동명 동물 수 찾기/solution.sql
@@ -1,2 +1,8 @@
 -- 코드를 입력하세요
 SELECT NAME, COUNT(*) AS "COUNT" FROM ANIMAL_INS WHERE NAME IS NOT NULL GROUP BY NAME HAVING COUNT(*) > 1 ORDER BY NAME;
+
+-- 코드를 입력하세요
+select name, count
+from (select name, count(*) as "count" from animal_ins group by name having name is not null) as sub_table
+where count >= 2
+order by name;
