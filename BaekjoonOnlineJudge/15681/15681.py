cc13167c145bf5d5c49d60d20404b1f2874e8349
hellominchan <wjdalscksdle@naver.com>
2022-03-12 20:26:19 +0900

    22.03.12

--- BaekjoonOnlineJudge/15681/15681.py
+++ BaekjoonOnlineJudge/15681/15681.py
@@ -1,22 +1,27 @@
 import sys
+sys.setrecursionlimit(10**7)
+
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 7)
 
-def DFS(root):
-    visit[root] = True
-    memoization[root] = 1
 
-    for childNode in tree[root]:
-        if not visit[childNode]:
-            DFS(childNode)
-            memoization[root] += memoization[childNode]
+def dfs(root):
+    child_count[root] = 1
 
-    return
+    for child_node in tree[root]:
+        if not visit[child_node]:
+            visit[child_node] = True
+
+            child_count[root] += dfs(child_node)
+
+    return child_count[root]
+    
+
 
 N, R, Q = map(int, input().split())
+
 tree = [[] for _ in range(N+1)]
-memoization = [0 for _ in range(N+1)]
 visit = [False for _ in range(N+1)]
+child_count = [0 for _ in range(N+1)]
 
 for _ in range(N-1):
     U, V = map(int, input().split())
@@ -24,8 +29,10 @@ for _ in range(N-1):
     tree[U].append(V)
     tree[V].append(U)
 
-DFS(R)
+visit[R] = True
+dfs(R)
 
 for _ in range(Q):
-    query = int(input())
-    print(memoization[query])
+    U = int(input())
+
+    print(child_count[U])
