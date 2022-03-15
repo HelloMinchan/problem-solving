a0ee67ba9c23c16c9a3417e77111974ef4b61ca2
hellominchan <wjdalscksdle@naver.com>
2022-03-15 16:19:51 +0900

    update README.md

--- BaekjoonOnlineJudge/2667/2667.py
+++ BaekjoonOnlineJudge/2667/2667.py
@@ -1,11 +1,15 @@
 from collections import deque
 import sys
+
 input = sys.stdin.readline
+
 
 def bfs(dq):
     count = 0
+
     while dq:
         count += 1
+
         i, j = dq.popleft()
 
         for way in range(4):
@@ -15,12 +19,14 @@ def bfs(dq):
             if ii < 0 or ii > N - 1 or jj < 0 or jj > N - 1:
                 continue
             
-            if not visit[ii][jj] and board[ii][jj] == "1":
+            if board[ii][jj] == "1" and not visit[ii][jj]:
                 visit[ii][jj] = True
-                dq.append((ii,jj))
 
-    answer.append(count)
+                dq.append((ii, jj))
+    
+    answer_list.append(count)
 
+    
 N = int(input())
 
 board = [list(input().rstrip()) for _ in range(N)]
@@ -28,18 +34,18 @@ visit = [[False for _ in range(N)] for _ in range(N)]
 
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
-answer = []
+
+answer_list = []
 
 for i in range(N):
     for j in range(N):
         if board[i][j] == "1" and not visit[i][j]:
             visit[i][j] = True
+
             dq = deque()
             dq.append((i,j))
-
             bfs(dq)
 
-print(len(answer))
-
-for count in sorted(answer):
-    print(count)
+print(len(answer_list))
+for answer in sorted(answer_list):
+    print(answer)
